b70ccd339dd6397e244412c72b03fdd392c0cc01
Harry Hopkinson <harryjhopkinson@gmail.com>
2022-03-22 20:59:41 +0000

    Print Board

--- src/main.py
+++ src/main.py
@@ -17,4 +17,41 @@ board = [
     [0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0],
 ]
-printBoard(board)
+
+
+def play(board, player, i, j):
+    if i < 0 or i > 4 or j < 0 or j > 4:
+        return False
+    if board[i][j] == 0:
+        board[i][j] = player
+        if player == 1:
+            player = -1
+        else:
+            player = 1
+        return player
+    else:
+        return False
+
+def endGame(board):
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if board[i][j] == 0:
+                return False
+    return True
+    
+while True:
+    endGame(board)
+    printBoard(board)
+    print("Player 1:")
+    i = int(input("Row: "))
+    j = int(input("Column: "))
+    endGame(board)
+    if play(board, 1, i, j):
+        print("Player 2:")
+        i = int(input("Row: "))
+        j = int(input("Column: "))
+        if play(board, -1, i, j):
+            pass
+    else:
+        print("Invalid move")
+
