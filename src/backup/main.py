bcc84d8ee4b9b757b6693324db5e74f837452e7e
Harry-Hopkinson <harryjhopkinson@gmail.com>
2022-03-22 20:53:21 +0000

    Print Board

--- src/backup/main.py
+++ src/backup/main.py
@@ -1,20 +1,31 @@
 import numpy as numpy
 from board import *
 
+
 class DotsAndBoxes(object):
-    def __init__(self, initial_player=None, rows=None, cols=None, board_string=None, game_obj=None):
-        if board_string is not None: # construct from string
+    def __init__(
+        self,
+        initial_player=None,
+        rows=None,
+        cols=None,
+        board_string=None,
+        game_obj=None,
+    ):
+        if board_string is not None:  # construct from string
             self._from_string(initial_player, board_string)
-        elif game_obj is not None:   # copy constructor
+        elif game_obj is not None:  # copy constructor
             self._copy(game_obj)
         else:
             self.rows, self.cols = rows, cols
-            self.score = [0, 0] # B, W
+            self.score = [0, 0]  # B, W
             self.turn = initial_player
-            self.board = np.zeros((2*rows-1, 2*cols-1), dtype=np.int)
+            self.board = np.zeros((2 * rows - 1, 2 * cols - 1), dtype=np.int)
             self.r, self.c = self.board.shape
-            self._edges = np.reshape([i%2 != j%2 for j in xrange(self.c) for i in xrange(self.r)], (self.r, self.c))
-            self.num_boxes = (rows-1) * (cols-1)
+            self._edges = np.reshape(
+                [i % 2 != j % 2 for j in xrange(self.c) for i in xrange(self.r)],
+                (self.r, self.c),
+            )
+            self.num_boxes = (rows - 1) * (cols - 1)
 
     def _from_string(self, initial_player, board_string):
         lines = board_string.split("|")
@@ -22,10 +33,10 @@ class DotsAndBoxes(object):
         self.__init__(initial_player, rows, cols)
         for i, r in enumerate(lines):
             for j, s in enumerate(r):
-                edge, box = i%2 != j%2, i%2 == j%2 == 1
-                if edge and s == 'x':      # is a filled edge
+                edge, box = i % 2 != j % 2, i % 2 == j % 2 == 1
+                if edge and s == "x":  # is a filled edge
                     self.board[i, j] += 1
-                elif box and s in PLAYERS: # is a filled box
+                elif box and s in PLAYERS:  # is a filled box
                     self.board[i, j] = PLAYERS[s]
                     self.score[PLAYERS[s] == 1] += 1
 
@@ -43,7 +54,7 @@ class DotsAndBoxes(object):
             self.board[i, j] = player
             self._update(player, i, j)
         else:
-            raise Exception('Invalid move %d,%d' % (i, j))
+            raise Exception("Invalid move %d,%d" % (i, j))
 
     def get_available_moves(self):
         unplayed = np.logical_not(self.board)
@@ -51,24 +62,40 @@ class DotsAndBoxes(object):
         return zip(moves[0], moves[1])
 
     def _is_valid(self, player, i, j):
-        return self.turn == player and self.board[i, j] == 0 and i%2 != j%2 and not self.is_over()
+        return (
+            self.turn == player
+            and self.board[i, j] == 0
+            and i % 2 != j % 2
+            and not self.is_over()
+        )
 
     def is_over(self):
         return sum(self.score) == self.num_boxes
 
     def _update(self, player, i, j):
-        vertical = i%2 > j%2
-        a = self._check_box(player, i, j-1) if vertical else self._check_box(player, i-1, j)
-        b = self._check_box(player, i, j+1) if vertical else self._check_box(player, i+1, j)
-        if not (a or b): # did not fill any box
+        vertical = i % 2 > j % 2
+        a = (
+            self._check_box(player, i, j - 1)
+            if vertical
+            else self._check_box(player, i - 1, j)
+        )
+        b = (
+            self._check_box(player, i, j + 1)
+            if vertical
+            else self._check_box(player, i + 1, j)
+        )
+        if not (a or b):  # did not fill any box
             self.turn *= -1
 
     def _check_box(self, player, i, j):
         if (i and i < self.r) and (j and j < self.c):
-            filled = self.board[i-1, j] and self.board[i+1, j] and self.board[i, j-1] and self.board[i, j+1]
+            filled = (
+                self.board[i - 1, j]
+                and self.board[i + 1, j]
+                and self.board[i, j - 1]
+                and self.board[i, j + 1]
+            )
             if filled:
                 self.board[i, j] = player
                 self.score[player == 1] += 1
             return filled
-
-
--- src/main.py
+++ src/main.py
@@ -1,12 +1,20 @@
-### print out a dots and boxes board
-
 def printBoard(board):
     for i in range(len(board)):
         for j in range(len(board[i])):
             if board[i][j] == 0:
-                print('.', end='')
+                print(".", end="")
             elif board[i][j] == 1:
-                print('x', end='')
+                print("x", end="")
             elif board[i][j] == -1:
-                print('o', end='')
+                print("o", end="")
         print()
+
+
+board = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0],
+]
+printBoard(board)
